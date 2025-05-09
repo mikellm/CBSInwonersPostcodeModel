0ae4bcd18c2251a406f48e6c6773c473d20423a8
Michael Damen <michael.damen@gmail.com>
2025-05-09 11:49:57 +0200

    lijst met packages als vector

--- setup.R
+++ setup.R
@@ -1,3 +1,5 @@
-install.packages("tidyverse", "tidymodels", "sf", "shiny","shinytest2", "shinyvalidate", "plotly", "leaflet", "htmltools", "waiter", "ranger")
+install.packages(c("tidyverse", "tidymodels", "sf", "shiny","shinytest2", "shinyvalidate", "plotly", "leaflet", "htmltools", "waiter", "ranger"), dependencies =  TRUE)
 # To run these tests you have to have Chrome installed
 chromote::find_chrome()
+
+install.packages("shinytest2", dependencies = TRUE)
